a5430934a98de95eacf1b27715813d5d922b76df
CMielczarek <mielczarekcasper@gmail.com>
2023-08-18 17:27:30 +0200

    nav, hero, first section and second section

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,27 @@ const plugins = Object.keys(allPlugins)
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,php}'],
+	content: ['./src/**/*.{html,js}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		fontFamily: {
+			montserrat: ['Montserrat', 'sans-serif'],
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1.5rem',
+				xl: '5rem',
+			},
+		},
+		extend: {
+			colors: {
+				lead: '#111111',
+				beige: '#DCC1AB',
+				'impure-white': '#F5F0EC',
+				green: '#1B5B31',
+			},
+		},
 	},
 	plugins: plugins,
 }
